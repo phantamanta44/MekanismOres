48f5832d007d190a7955442bb24ae5690b5b5eac
phantamanta44 <evandalong@gmail.com>
2017-08-01 10:24:32 -0500

    Filter for items atlas

--- src/main/java/io/github/phantamanta44/mekores/client/ClientEventListener.java
+++ src/main/java/io/github/phantamanta44/mekores/client/ClientEventListener.java
@@ -6,10 +6,12 @@ public class ClientEventListener {
 
     @SubscribeEvent
     public void onTextureStitch(TextureStitchEvent.Post event) {
-        int tex = event.getMap().getGlTextureId();
-        ByteBuffer buf = ByteBuffer.allocate(); // FIXME find some hacky way to get the texture size;
-        GL11.glGetTexImage(tex, 0, GL11.GL_RGBA, GL11.GL_BYTE, buf);
-        atlas = buf.toArray();
+        if (event.getMap().basePath.equals("items")) { // FIXME find a way to make sure we're on the items atlas
+            int tex = event.getMap().getGlTextureId();
+            ByteBuffer buf = ByteBuffer.allocate(); // FIXME find some hacky way to get the texture size; maybe bind a delegating logger to System.out and intercept the log message?
+            GL11.glGetTexImage(tex, 0, GL11.GL_RGBA, GL11.GL_BYTE, buf);
+            atlas = buf.toArray();
+        }
     }
 
     public static byte[] getAtlas() {
